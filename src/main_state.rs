ec1953a6534e62d437131c5ece8300b091371cc4
Andrew Minnich <aminnich3@gmail.com>
2024-02-17 00:53:06 -0500

    allow rendering to video

--- src/main_state.rs
+++ src/main_state.rs
@@ -1,38 +1,42 @@
-mod sound_state;
 mod ui;
 
-use std::time::Duration;
+use std::{
+	io::Write,
+	process::{Child, Command, Stdio},
+	time::Duration,
+};
 
 use glam::Vec2;
 use kira::{
 	manager::{AudioManager, AudioManagerSettings},
 	sound::{
-		streaming::{StreamingSoundData, StreamingSoundSettings},
-		PlaybackPosition, PlaybackState,
+		streaming::{StreamingSoundData, StreamingSoundHandle, StreamingSoundSettings},
+		FromFileError, PlaybackPosition, PlaybackState,
 	},
 	tween::Tween,
 };
 use micro::{
-	graphics::{Canvas, CanvasSettings, ColorConstants, DrawParams},
+	graphics::{Canvas, CanvasSettings, ColorConstants, DrawParams, SwapInterval},
 	input::Scancode,
 	Context, Event, State,
 };
 use palette::LinSrgba;
+use rfd::FileDialog;
 
 use crate::{Chapter, Visualizer};
-
-use self::sound_state::SoundState;
 
 const FINISHED_SEEK_DETECTION_THRESHOLD: f64 = 0.1;
 
 pub struct MainState {
 	visualizer: Box<dyn Visualizer>,
 	audio_manager: AudioManager,
-	sound_state: SoundState,
+	mode: Mode,
 	duration: Duration,
 	previous_position: f64,
 	chapters: Vec<Chapter>,
 	canvas: Canvas,
+	rendering_settings: RenderingSettings,
+	show_rendering_window: bool,
 }
 
 impl MainState {
@@ -49,10 +53,18 @@ impl MainState {
 			visualizer.video_resolution(),
 			CanvasSettings::default(),
 		);
+		let rendering_settings = if chapters.is_empty() {
+			RenderingSettings::default()
+		} else {
+			RenderingSettings {
+				start_chapter_index: 0,
+				end_chapter_index: chapters.len() - 1,
+			}
+		};
 		Ok(MainState {
 			visualizer,
 			audio_manager,
-			sound_state: SoundState::Stopped {
+			mode: Mode::Stopped {
 				data: Some(sound_data),
 				start_position: 0.0,
 			},
@@ -60,38 +72,68 @@ impl MainState {
 			previous_position: 0.0,
 			chapters,
 			canvas,
+			rendering_settings,
+			show_rendering_window: false,
 		})
 	}
 
+	fn frame_to_time(&self, frame: u64) -> f64 {
+		frame as f64 / self.visualizer.frame_rate() as f64
+	}
+
+	fn num_frames(&self) -> u64 {
+		(self.duration.as_secs_f64() * self.visualizer.frame_rate() as f64).ceil() as u64
+	}
+
+	fn playing(&self) -> bool {
+		match &self.mode {
+			Mode::Stopped { .. } => false,
+			Mode::PlayingOrPaused { sound, .. } => sound.state() == PlaybackState::Playing,
+			Mode::Rendering { .. } => false,
+		}
+	}
+
+	fn current_position(&self) -> f64 {
+		match &self.mode {
+			Mode::Stopped { start_position, .. } => *start_position,
+			Mode::PlayingOrPaused {
+				sound,
+				in_progress_seek,
+			} => in_progress_seek.unwrap_or_else(|| sound.position()),
+			Mode::Rendering { current_frame, .. } => self.frame_to_time(*current_frame),
+		}
+	}
+
 	fn play_or_resume(&mut self) -> anyhow::Result<()> {
-		match &mut self.sound_state {
-			SoundState::Stopped {
+		match &mut self.mode {
+			Mode::Stopped {
 				data,
 				start_position,
 			} => {
 				let mut data = data.take().unwrap();
 				data.settings.start_position = PlaybackPosition::Seconds(*start_position);
-				self.sound_state = SoundState::PlayingOrPaused {
+				self.mode = Mode::PlayingOrPaused {
 					sound: self.audio_manager.play(data)?,
 					in_progress_seek: None,
 				};
 			}
-			SoundState::PlayingOrPaused { sound, .. } => {
+			Mode::PlayingOrPaused { sound, .. } => {
 				sound.resume(Tween::default())?;
 			}
+			Mode::Rendering { .. } => unreachable!("not supported in rendering mode"),
 		}
 		Ok(())
 	}
 
 	fn pause(&mut self) -> anyhow::Result<()> {
-		if let SoundState::PlayingOrPaused { sound, .. } = &mut self.sound_state {
+		if let Mode::PlayingOrPaused { sound, .. } = &mut self.mode {
 			sound.pause(Tween::default())?;
 		}
 		Ok(())
 	}
 
 	fn toggle_playback(&mut self) -> anyhow::Result<()> {
-		if self.sound_state.playing() {
+		if self.playing() {
 			self.pause()?;
 		} else {
 			self.play_or_resume()?;
@@ -100,23 +142,24 @@ impl MainState {
 	}
 
 	fn seek(&mut self, position: f64) -> anyhow::Result<()> {
-		match &mut self.sound_state {
-			SoundState::Stopped { start_position, .. } => {
+		match &mut self.mode {
+			Mode::Stopped { start_position, .. } => {
 				*start_position = position;
 			}
-			SoundState::PlayingOrPaused {
+			Mode::PlayingOrPaused {
 				sound,
 				in_progress_seek,
 			} => {
 				sound.seek_to(position)?;
 				*in_progress_seek = Some(position);
 			}
+			Mode::Rendering { .. } => unreachable!("not supported in rendering mode"),
 		}
 		Ok(())
 	}
 
 	fn current_frame(&self) -> u64 {
-		(self.sound_state.current_position() * self.visualizer.frame_rate() as f64).ceil() as u64
+		(self.current_position() * self.visualizer.frame_rate() as f64).ceil() as u64
 	}
 
 	fn current_chapter_index(&self) -> Option<usize> {
@@ -128,10 +171,16 @@ impl MainState {
 			.map(|(index, _)| index)
 	}
 
+	fn chapter_end_frame(&self, chapter_index: usize) -> u64 {
+		self.chapters
+			.get(chapter_index + 1)
+			.map(|chapter| chapter.start_frame - 1)
+			.unwrap_or(self.num_frames() - 1)
+	}
+
 	fn go_to_chapter(&mut self, chapter_index: usize) -> anyhow::Result<()> {
 		let chapter = &self.chapters[chapter_index];
-		let chapter_start_position =
-			chapter.start_frame as f64 / self.visualizer.frame_rate() as f64;
+		let chapter_start_position = self.frame_to_time(chapter.start_frame);
 		self.seek(chapter_start_position)?;
 		Ok(())
 	}
@@ -159,11 +208,88 @@ impl MainState {
 		self.go_to_chapter(current_chapter_index - 1)?;
 		Ok(())
 	}
+
+	fn render(&mut self, ctx: &mut Context) -> anyhow::Result<()> {
+		let Some(video_path) = FileDialog::new()
+			.set_directory(std::env::current_exe().unwrap())
+			.add_filter("mp4 video", &["mp4"])
+			.save_file()
+		else {
+			return Ok(());
+		};
+		let (start_frame, end_frame) = if !self.chapters.is_empty() {
+			let start_frame =
+				self.chapters[self.rendering_settings.start_chapter_index].start_frame;
+			let end_frame = self.chapter_end_frame(self.rendering_settings.end_chapter_index);
+			(start_frame, end_frame)
+		} else {
+			(0, self.num_frames() - 1)
+		};
+		let start_time = self.frame_to_time(start_frame);
+		let ffmpeg_process = Command::new("ffmpeg")
+			.stdin(Stdio::piped())
+			.arg("-y")
+			.arg("-f")
+			.arg("rawvideo")
+			.arg("-vcodec")
+			.arg("rawvideo")
+			.arg("-s")
+			.arg(&format!(
+				"{}x{}",
+				self.visualizer.video_resolution().x,
+				self.visualizer.video_resolution().y
+			))
+			.arg("-pix_fmt")
+			.arg("rgba")
+			.arg("-r")
+			.arg(self.visualizer.frame_rate().to_string())
+			.arg("-i")
+			.arg("-")
+			.arg("-ss")
+			.arg(&format!("{}s", start_time))
+			.arg("-i")
+			.arg(self.visualizer.audio_path())
+			.arg("-b:a")
+			.arg("320k")
+			.arg("-c:v")
+			.arg("libx264")
+			.arg("-r")
+			.arg(self.visualizer.frame_rate().to_string())
+			.arg("-shortest")
+			.arg(video_path)
+			.spawn()?;
+		let canvas_read_buffer = vec![
+			0;
+			(self.visualizer.video_resolution().x * self.visualizer.video_resolution().y * 4)
+				as usize
+		];
+		self.mode = Mode::Rendering {
+			end_frame,
+			current_frame: start_frame,
+			canvas_read_buffer,
+			ffmpeg_process,
+		};
+		ctx.set_swap_interval(SwapInterval::Immediate)?;
+		Ok(())
+	}
+
+	fn stop_rendering(&mut self, ctx: &mut Context) -> Result<(), anyhow::Error> {
+		self.mode = Mode::Stopped {
+			data: Some(StreamingSoundData::from_file(
+				self.visualizer.audio_path(),
+				StreamingSoundSettings::default(),
+			)?),
+			start_position: 0.0,
+		};
+		ctx.set_swap_interval(SwapInterval::VSync)?;
+		Ok(())
+	}
 }
 
 impl State<anyhow::Error> for MainState {
-	fn ui(&mut self, _ctx: &mut Context, egui_ctx: &egui::Context) -> Result<(), anyhow::Error> {
+	fn ui(&mut self, ctx: &mut Context, egui_ctx: &egui::Context) -> Result<(), anyhow::Error> {
 		self.render_main_menu(egui_ctx)?;
+		self.render_rendering_window(ctx, egui_ctx)?;
 		Ok(())
 	}
 
@@ -193,10 +319,10 @@ impl State<anyhow::Error> for MainState {
 	}
 
 	fn update(&mut self, _ctx: &mut Context, _delta_time: Duration) -> Result<(), anyhow::Error> {
-		if let SoundState::PlayingOrPaused {
+		if let Mode::PlayingOrPaused {
 			sound,
 			in_progress_seek,
-		} = &mut self.sound_state
+		} = &mut self.mode
 		{
 			if let Some(in_progress_seek_destination) = in_progress_seek {
 				if (sound.position() - *in_progress_seek_destination).abs()
@@ -206,7 +332,7 @@ impl State<anyhow::Error> for MainState {
 				}
 			}
 			if sound.state() == PlaybackState::Stopped {
-				self.sound_state = SoundState::Stopped {
+				self.mode = Mode::Stopped {
 					data: Some(StreamingSoundData::from_file(
 						self.visualizer.audio_path(),
 						StreamingSoundSettings::default(),
@@ -221,10 +347,10 @@ impl State<anyhow::Error> for MainState {
 	fn draw(&mut self, ctx: &mut Context) -> Result<(), anyhow::Error> {
 		ctx.clear(LinSrgba::BLACK);
 		let current_frame = self.current_frame();
-		if self.sound_state.current_position() != self.previous_position {
+		if self.current_position() != self.previous_position {
 			let ctx = &mut self.canvas.render_to(ctx);
 			self.visualizer.draw(ctx, current_frame)?;
-			self.previous_position = self.sound_state.current_position();
+			self.previous_position = self.current_position();
 		}
 		let max_horizontal_scale =
 			ctx.window_size().x as f32 / self.visualizer.video_resolution().x as f32;
@@ -238,6 +364,49 @@ impl State<anyhow::Error> for MainState {
 				.scaled_2d(Vec2::splat(scale))
 				.translated_2d(ctx.window_size().as_vec2() / 2.0),
 		);
+		if let Mode::Rendering {
+			end_frame,
+			current_frame,
+			canvas_read_buffer,
+			ffmpeg_process,
+		} = &mut self.mode
+		{
+			self.canvas.read(ctx, canvas_read_buffer);
+			let ffmpeg_stdin = ffmpeg_process.stdin.as_mut().unwrap();
+			let write_result = ffmpeg_stdin.write_all(canvas_read_buffer);
+			if write_result.is_err() {
+				self.stop_rendering(ctx)?;
+			} else {
+				*current_frame += 1;
+				if *current_frame > *end_frame {
+					self.stop_rendering(ctx)?;
+				}
+			}
+		}
 		Ok(())
 	}
+}
+
+#[allow(clippy::large_enum_variant)]
+enum Mode {
+	Stopped {
+		data: Option<StreamingSoundData<FromFileError>>,
+		start_position: f64,
+	},
+	PlayingOrPaused {
+		sound: StreamingSoundHandle<FromFileError>,
+		in_progress_seek: Option<f64>,
+	},
+	Rendering {
+		end_frame: u64,
+		current_frame: u64,
+		canvas_read_buffer: Vec<u8>,
+		ffmpeg_process: Child,
+	},
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+struct RenderingSettings {
+	start_chapter_index: usize,
+	end_chapter_index: usize,
 }
--- src/main_state/ui.rs
+++ src/main_state/ui.rs
@@ -1,6 +1,7 @@
-use egui::{ComboBox, Slider, TopBottomPanel, Ui};
+use egui::{ComboBox, InnerResponse, Slider, TopBottomPanel, Ui};
+use micro::Context;
 
-use super::MainState;
+use super::{MainState, Mode};
 
 impl MainState {
 	pub fn render_main_menu(&mut self, egui_ctx: &egui::Context) -> Result<(), anyhow::Error> {
@@ -8,13 +9,18 @@ impl MainState {
 			.show(egui_ctx, |ui| -> anyhow::Result<()> {
 				egui::menu::bar(ui, |ui| -> anyhow::Result<()> {
 					self.render_play_pause_button(ui)?;
+					if ui.button("Render").clicked() {
+						self.show_rendering_window = true;
+					}
 					self.render_seekbar(ui)?;
 					self.render_chapter_combo_box(ui)?;
-					if ui.button("<<").clicked() {
-						self.go_to_previous_chapter()?;
-					}
-					if ui.button(">>").clicked() {
-						self.go_to_next_chapter()?;
+					if !matches!(self.mode, Mode::Rendering { .. }) {
+						if ui.button("<<").clicked() {
+							self.go_to_previous_chapter()?;
+						}
+						if ui.button(">>").clicked() {
+							self.go_to_next_chapter()?;
+						}
 					}
 					Ok(())
 				})
@@ -24,12 +30,48 @@ impl MainState {
 		Ok(())
 	}
 
+	pub fn render_rendering_window(
+		&mut self,
+		ctx: &mut Context,
+		egui_ctx: &egui::Context,
+	) -> anyhow::Result<()> {
+		let response = egui::Window::new("Rendering")
+			.open(&mut self.show_rendering_window)
+			.show(egui_ctx, |ui| {
+				let mut rendering_started = false;
+				if !self.chapters.is_empty() {
+					ComboBox::new("start_chapter_index", "Start Chapter Index").show_index(
+						ui,
+						&mut self.rendering_settings.start_chapter_index,
+						self.chapters.len(),
+						|i| &self.chapters[i].name,
+					);
+					ComboBox::new("end_chapter_index", "End Chapter Index").show_index(
+						ui,
+						&mut self.rendering_settings.end_chapter_index,
+						self.chapters.len(),
+						|i| &self.chapters[i].name,
+					);
+				}
+				if ui.button("Render").clicked() {
+					rendering_started = true;
+				}
+				rendering_started
+			});
+		if let Some(InnerResponse {
+			inner: Some(true), ..
+		}) = response
+		{
+			self.render(ctx)?;
+		}
+		Ok(())
+	}
+
 	fn render_play_pause_button(&mut self, ui: &mut Ui) -> Result<(), anyhow::Error> {
-		let play_pause_button_text = if self.sound_state.playing() {
-			"Pause"
-		} else {
-			"Play"
-		};
+		if matches!(self.mode, Mode::Rendering { .. }) {
+			return Ok(());
+		}
+		let play_pause_button_text = if self.playing() { "Pause" } else { "Play" };
 		if ui.button(play_pause_button_text).clicked() {
 			self.toggle_playback()?;
 		};
@@ -37,21 +79,19 @@ impl MainState {
 	}
 
 	fn render_seekbar(&mut self, ui: &mut Ui) -> Result<(), anyhow::Error> {
-		let mut position = self.sound_state.current_position();
-		if ui
-			.add(
-				Slider::new(&mut position, 0.0..=self.duration.as_secs_f64()).custom_formatter(
-					|position, _| {
-						format!(
-							"{} / {}",
-							format_position(position),
-							format_position(self.duration.as_secs_f64())
-						)
-					},
-				),
-			)
-			.drag_released()
-		{
+		let mut position = self.current_position();
+		let slider_response = &ui.add(
+			Slider::new(&mut position, 0.0..=self.duration.as_secs_f64()).custom_formatter(
+				|position, _| {
+					format!(
+						"{} / {}",
+						format_position(position),
+						format_position(self.duration.as_secs_f64())
+					)
+				},
+			),
+		);
+		if slider_response.drag_released() && !matches!(self.mode, Mode::Rendering { .. }) {
 			self.seek(position)?;
 		};
 		Ok(())
@@ -59,6 +99,11 @@ impl MainState {
 
 	fn render_chapter_combo_box(&mut self, ui: &mut Ui) -> anyhow::Result<()> {
 		if self.chapters.is_empty() {
+			return Ok(());
+		}
+		if matches!(self.mode, Mode::Rendering { .. }) {
+			let chapter = &self.chapters[self.current_chapter_index().expect("no current chapter")];
+			ui.label(&chapter.name);
 			return Ok(());
 		}
 		let mut selected = self.current_chapter_index().expect("no current chapter");
